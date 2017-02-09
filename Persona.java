e80d97b52d777d030efe9c4b0af36a84a24b600c
LuisGonzalez93 <lg02393@gmail.com>
2017-02-09 13:28:49 +0100

    Metodo contestar añadido

--- Persona.java
+++ Persona.java
@@ -15,7 +15,8 @@ public class Persona
     private int caloriasIngeridas;
     private boolean hombre;
     private Comida comida;
-
+    private int metabolismoBasal;
+    private boolean sobrecargado;
     /**
      * Introduce los datos de Persona
      */
@@ -27,28 +28,55 @@ public class Persona
         edad = Edad;
         caloriasIngeridas = 0;
         hombre = Hombre;
+        sobrecargado = false;
+           if(hombre == true){
+            metabolismoBasal= (10*peso)+(6*altura)+(5*edad)+5;}
+            else{
+            metabolismoBasal= (10*peso)+(6*altura)+(5*edad)-161;
+            }
     }
 
     public int comer(Comida comida){
         int ingerir = -1;
         int caloriaComida = comida.getCalorias();
         if(hombre == true && comida!=null){
-            if((10*peso)+(6*altura)+(5*edad)+5>=caloriasIngeridas){
+            if(metabolismoBasal>=caloriasIngeridas){
                 ingerir = caloriaComida;
                 caloriasIngeridas = caloriasIngeridas + caloriaComida;
+                
             }
-                    }
+        }
         if(hombre == false && comida!=null){
-            if((10*peso)+(6*altura)+(5*edad)-161>=caloriasIngeridas){
+            if(metabolismoBasal>=caloriasIngeridas){
                 ingerir = caloriaComida;
                 caloriasIngeridas = caloriasIngeridas + caloriaComida;
+                
             }
-                    }         
-                   
+        }         
+        while(metabolismoBasal<=caloriasIngeridas){
+        sobrecargado = true;
+        }
         return ingerir;
     }
 
     public int getCaloriasIngeridas(){
         return caloriasIngeridas;
+    }
+
+    public String contestar(String pregunta){
+        String respuesta = "";
+        
+        if((pregunta.length()%3)==0 && sobrecargado == false){
+            respuesta="SI";
+        }
+        if((pregunta.length()%3)!=0 && sobrecargado == false){
+            respuesta="NO";
+        }
+        if(pregunta.contains(nombrePersona)||sobrecargado == true){
+            respuesta = pregunta.toUpperCase();
+            System.out.println(pregunta.toUpperCase());
+        }
+       
+        return respuesta;
     }
 }
